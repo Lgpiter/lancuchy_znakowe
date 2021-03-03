8ef4cfce39831b1c1a967b33e1bb0fb9d382955f
Lgpiter <78417247+Lgpiter@users.noreply.github.com>
2021-03-04 00:27:32 +0100

    Add files via upload

--- Zadanie10.c
+++ Zadanie10.c
@@ -42,6 +42,9 @@ int main()
         case 4:  wypisz_lanuchy_od_dlugosci_pierwszego_slowa(wsklan,licznik);
                  scanf("%d",&wybor);
                  break;
+        default: printf("Podales zla liczbe. Sprobuj jeszcze raz\n");
+                 scanf("%d",&wybor);
+                 break;
         }
             
                 
@@ -114,15 +117,13 @@ void wypisz_lancuchy_rosnaco(char *lancuchy[], int num)
     
     for(dol = 0; dol <num; dol++)
     {
-        dlugosc1 = strlen(lancuchy[dol]);
-        printf("Dlugosc pierwszego %d\n",dlugosc1);
+  
         for(szuk = dol + 1; szuk < num; szuk ++)
         {
+            dlugosc1 = strlen(lancuchy[dol]);
             dlugosc2 = strlen(lancuchy[szuk]);
-            printf("Dlugosc drugiego %d\n",dlugosc2);
             if (dlugosc1 >= dlugosc2)
             {
-                printf("Zamieniam %d z %d\n",dol,szuk);
                 temp = lancuchy[dol];
                 lancuchy[dol] = lancuchy[szuk];
                 lancuchy[szuk] = temp;
@@ -145,21 +146,22 @@ void wypisz_lanuchy_od_dlugosci_pierwszego_slowa(char *lancuchy[], int num)
     int k = 0;
     for(dol = 0; dol <num; dol++)
     {
-        while(lancuchy[dol][j] != '\0')
-        {
-            if(isspace(lancuchy[dol][j]))
-            {
-                break;
-            }
-            dlugosc1++;
-            j++;
-        }
         
         for(szuk = dol + 1; szuk < num; szuk ++)
         {
+            while(lancuchy[dol][j] != '\0')
+            {
+                if(isspace(lancuchy[dol][j]))
+                {
+                    break;
+                }
+                dlugosc1++;
+                j++;
+            }
+        
             while(lancuchy[szuk][k] != '\0')
             {
-                if(isspace(lancuchy[szuk][j]))
+                if(isspace(lancuchy[szuk][k]))
                     break;
                 dlugosc2++;
                 k++;
@@ -167,16 +169,16 @@ void wypisz_lanuchy_od_dlugosci_pierwszego_slowa(char *lancuchy[], int num)
                
             if (dlugosc1 >= dlugosc2)
             {
-                printf("Dlugosc pierwszego: %d Dlugosc drugiego %d\n",dlugosc1,dlugosc2);
                 temp = lancuchy[dol];
                 lancuchy[dol] = lancuchy[szuk];
                 lancuchy[szuk] = temp;
             }
             k = 0;
             dlugosc2 = 0;
+            j = 0;
+            dlugosc1 = 0;
         }
-        j = 0;
-        dlugosc1 = 0;
+
     }
     for(int k = 0; k < num; k++)
     {
